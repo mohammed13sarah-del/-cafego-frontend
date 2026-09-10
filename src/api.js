06519297c85ab36d0499ff191df66183e46166ef
mohammed13sarah-del <@example.com>
2026-09-10 12:40:17 +0300

    update API URL

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 // 1. الرابط الأساسي للباك إند (تأكد من البورت 5182 كما في ملف launchSettings)
-const API_BASE_URL = "https://localhost:7203/api";
-
+const API_BASE_URL = "https://mohammed13sarah-001-site1.itempurl.com/api";
 const api = axios.create({
     baseURL: API_BASE_URL,
 });
